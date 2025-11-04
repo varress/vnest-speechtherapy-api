6d87e21b789768ffc0da375749348b36f82a5dcb
varress <lahteenmakiessi@hotmail.fi>
2025-11-04 21:54:33 +0200

    Require admin role for other than api endpoints

--- src/main/java/fi/vnest/speechtherapy/api/config/SecurityConfig.java
+++ src/main/java/fi/vnest/speechtherapy/api/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import fi.vnest.speechtherapy.api.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,8 +25,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login", "/css/**", "/js/**", "/error", "/api/**").permitAll()
+                        .requestMatchers("/login", "/error").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/*.css", "/*.js").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers("/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
